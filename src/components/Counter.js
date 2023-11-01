c8ea5666ceb85cc06dfadef76ea6667113b24158
MikailAlvares <mikailismail260@gmail.com>
2023-11-01 21:53:07 +0300

    completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,7 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
